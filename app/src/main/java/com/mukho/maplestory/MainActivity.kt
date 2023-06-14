30c5c6c270d1c2e583e0c74a707c15853c40ebd4
Mukho <mukho@kakao.com>
2023-06-14 22:43:52 +0900

    Style: Update Layout

--- app/src/main/java/com/mukho/maplestory/MainActivity.kt
+++ app/src/main/java/com/mukho/maplestory/MainActivity.kt
@@ -2,10 +2,12 @@ package com.mukho.maplestory
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContent(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        setContentView(R.layout.activity_main)
     }
 }
